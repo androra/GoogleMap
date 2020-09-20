7c2d355bea31a29efe4ab7a0c1bdd1bef9092e57
Bhupendrapatel98 <bhupendrapatel.bp57@gmail.com>
2020-09-21 01:53:47 +0530

    open Default Country

--- app/src/main/java/com/example/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/googlemap/MapsActivity.java
@@ -28,6 +28,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -38,6 +39,7 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
@@ -80,60 +82,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng Andaman = new LatLng(11.66702557, 92.73598262);
-        LatLng Andhra  = new LatLng(14.7504291,78.57002559);
-        LatLng Arunachal = new LatLng(27.10039878	,93.61660071);
-        LatLng Assam  = new LatLng(26.7499809,	94.21666744);
-        LatLng Bihar = new LatLng(25.78541445,	87.4799727);
-        LatLng Delhi  = new LatLng(	28.6699929,	77.23000403);
-        LatLng bhopal = new LatLng(23.2599333, 77.412615);
-        LatLng jaipur = new LatLng(26.9124336, 75.7872709);
+
+       //default open India map
+        LatLngBounds boundsIndia = new LatLngBounds(new LatLng(23.63936, 68.14712), new LatLng(28.20453, 97.34466));
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.10);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(boundsIndia, width, height, padding);
+        mMap.animateCamera(cameraUpdate);
+
+        LatLng ind = new LatLng(23.63936, 68.14712);
+        LatLng ind2 = new LatLng(28.20453, 97.34466);
+
 
         //add marker
-        mMap.addMarker(new MarkerOptions().position(bhopal).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(jaipur).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Andaman).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Andhra).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Arunachal).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Assam).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Bihar).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(Delhi).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bhopal));
-
-        //Polylines are useful for marking paths and routes on the map.
-         mMap.addPolyline(new PolylineOptions()
-                .add(new LatLng(23.2599333, 77.412615),
-                        new LatLng(25.78541445, 87.4799727),
-                        new LatLng(28.6699929, 77.23000403),
-                        new LatLng(26.9124336, 75.7872709),
-                        new LatLng(14.7504291, 78.57002559))
-                .width(5)
-                .color(Color.RED));
+        mMap.addMarker(new MarkerOptions().position(ind).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(ind2).title("Marker in Sydney"));
 
 
-         //add circle in perticular location
-       mMap.addCircle(new CircleOptions()
-                .center(new LatLng(23.2599333, 77.412615))
-                .radius(10000)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.BLUE));
-
-       //adpolygone for cover area
-       mMap.addPolygon(new PolygonOptions()
-       .add(new LatLng(23.2599333, 77.412615),
-               new LatLng(25.78541445, 87.4799727),
-               new LatLng(28.6699929, 77.23000403),
-               new LatLng(26.9124336, 75.7872709),
-               new LatLng(14.7504291, 78.57002559))
-       .fillColor(Color.GREEN)
-       .strokeColor(Color.RED));
-
-      //Ground overlays are useful when you wish to fix a single image at one area on the map
-        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.drawable.mark);
-        GroundOverlayOptions groundOverlay = new GroundOverlayOptions()
-                .image(image)
-                .position(bhopal, 8600f, 6500f);
-        mMap.addGroundOverlay(groundOverlay);
 
     }
 
